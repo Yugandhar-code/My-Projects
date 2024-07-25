44533d5bcd99189a0867ed27119f940306565a47
Yugandhar-code <musalamadugu.yugandhar@gmail.com>
2024-07-25 13:41:06 +0530

    Update Quiz.java

--- Quiz-Application/Quiz.java
+++ Quiz-Application/Quiz.java
@@ -1,1 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+    private List<Question> questions;
+    private int score;
+
+    public Quiz() {
+        questions = new ArrayList<>();
+        score = 0;
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void incrementScore() {
+        score++;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
+
 
