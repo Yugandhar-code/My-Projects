a2bbb438a0abf6caeca4dc05b3fedb3277a081ee
Yugandhar-code <musalamadugu.yugandhar@gmail.com>
2024-07-25 13:39:50 +0530

    Update Main.java

--- Quiz-Application/Main.java
+++ Quiz-Application/Main.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Quiz quiz = new Quiz();
+
+        quiz.addQuestion(new Question("What is the capital of France?", new String[]{"Paris", "London", "Rome", "Berlin"}, 1));
+        quiz.addQuestion(new Question("What is 2 + 2?", new String[]{"3", "4", "5", "6"}, 2));
+        quiz.addQuestion(new Question("What is the color of the sky?", new String[]{"Green", "Blue", "Red", "Yellow"}, 2));
+
+        System.out.println("Welcome to the Quiz!");
+        System.out.println("Please select the correct answer by entering the corresponding number.\n");
+
+        for (Question question : quiz.getQuestions()) {
+            System.out.println(question.getQuestion());
+            String[] options = question.getOptions();
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ": " + options[i]);
+            }
+            int userAnswer = scanner.nextInt();
+            if (userAnswer == question.getCorrectAnswer()) {
+                quiz.incrementScore();
+            }
+        }
+
+        System.out.println("\nYou completed the quiz!");
+        System.out.println("Your score: " + quiz.getScore() + "/" + quiz.getQuestions().size());
+    }
+}
+
 
